d5d5fefe96c5802a46563335abf7fa6724489866
Danny Ou <idannyou@gmail.com>
2019-01-14 21:29:49 -0800

    Create RepoList

---
+++ RepoList.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Flatlist, StyleSheet, Text, View } from 'react-native';
+import { connect } from 'react-redux';
+
+import { listRepos } from './Reducer';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  item: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: 'black'
+  }
+});
+
+class RepoList extends Component {
+  componentDidMount() {
+    this.props.listRepos('idannyou');
+  }
+  renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text>{item.name}</Text>
+    </View>
+  );
+  render() {
+    const { repos } = this.props;
+
+    return (
+      <FlatList
+        styles={styles.container}
+        data={repos}
+        renderItem={this.renderItem}
+      />
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    repos: state.repos.map(repo => ({ key: repo.id, ...repo }))
+  };
+};
+
+const mapDispatchToProps = {
+  listRepos
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RepoList);
