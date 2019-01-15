8641e98f1bb2f0cbdd34f25b5ea8dc8984d593dd
Danny Ou <idannyou@gmail.com>
2019-01-14 21:21:20 -0800

    Create Reducer

---
+++ Reducer.js
@@ -0,0 +1,35 @@
+export const GET_REPOS = 'GET_REPOS';
+export const GET_REPOS_SUCCESS = 'GET_REPOS_SUCCESS';
+export const GET_REPOS_FAIL = 'GET_REPOS_FAIL';
+
+const initalState = {
+  loading: false,
+  repos: []
+};
+
+export default function reducer(state = initalState, action) {
+  switch (action.type) {
+    case GET_REPOS:
+      return { ...state, loading: true };
+    case GET_REPOS_SUCCESS:
+      return { ...state, loading: false, repos: action.payload.data };
+    case GET_REPOS_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: 'Error'
+      };
+    default:
+      return state;
+  }
+}
+
+export function listRepos(user) {
+  return {
+    payload: {
+      request: {
+        url: `/users/${user}/repos`
+      }
+    }
+  };
+}
